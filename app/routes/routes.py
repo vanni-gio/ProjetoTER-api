a79ba36bb772ea4ab203846aabf5d0925061d0d2
vanni-gio <giovannip.gomesss@gmail.com>
2022-07-21 00:20:14 -0300

    definido os posts

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,3 +1,4 @@
+from glob import escape
 import jwt
 from requests import post
 from app import app
@@ -8,25 +9,28 @@ from app.routes.decorators import token_required
 @app.route('/certificado')
 @token_required
 def certificado(current_sensor):
-    response = post('127.0.0.1:5001', data=current_sensor)
-    return jsonify({'certificado': 'Hello World!'}), 200
+    response = post('127.0.0.1:5001/opensslertificate', data=current_sensor)
+    if response.status_code == 200:
+        return jsonify({'msg': 'autenticado', 'certificado': response.json()['certificado']}), 200
+    return jsonify({'msg': 'credenciais invalidas','certificado': None}), 400
 
-@app.route('/teste')
-def teste():
-    return jsonify({'message': 'Hello World!'}), 200
+# @app.route('/teste')
+# def teste():
+#     return jsonify({'message': 'Hello World!'}), 200
 
 @app.route('/token', methods=['POST'])
 def token():
     auth:dict = request.get_json()
-    if not auth or not auth.get('nomesensor') or not auth.get('senha'):
+    if not auth or not auth.get('nome_sensor') or not auth.get('senha'):
         return make_response('Could not verify!', 401, {'WWW-Authenticate': 'Basic-realm= "Login required!"'})
     # User.query.filter_by(username=auth['username']).first()
-    sensor = {'id': 1, 'senha': 'pbkdf2:sha256:260000$XmSIHyMRs4bGAeQO$188052f7a16fc877ca4d0905c9dbec7f11b41cd150f32fa28f758fb6edba0731'} # Buscar sensor no banco de dados e retornar id e senha
-    if not sensor:
-        return make_response('Could not verify user, Please signup!', 401, {'WWW-Authenticate': 'Basic-realm= "No user found!"'})
-
-    if check_password_hash(sensor.get('senha'), auth.get('senha')):
-        token = jwt.encode({'public_id': sensor.get('id')}, app.config['SECRET_KEY'], 'HS256')
+    response = post('127.0.0.1:5001/sensor', json={'nome': auth.get('nome_sensor')}) # Buscar sensor no banco de dados e retornar id e senha
+    
+    if response.status_code != 200:
+        return make_response('Could not verify sensor', response.status_code, {'WWW-Authenticate': 'Basic-realm= "No user found!"'})
+    sensor = response.json()
+    if check_password_hash(sensor['senha'], auth.get('senha')):
+        token = jwt.encode({'public_id': sensor['id']}, app.config['SECRET_KEY'], 'HS256')
         return make_response(jsonify({'token': token}), 201)
 
     return make_response('Could not verify password!', 403, {'WWW-Authenticate': 'Basic-realm= "Wrong Password!"'})
--- hashpw.py
+++
@@ -1,7 +0,0 @@
-pw = '12345'
-
-from werkzeug.security import check_password_hash,generate_password_hash
-
-pw_hsh = generate_password_hash(pw)
-print(pw_hsh)
-print(check_password_hash(pw_hsh, '12345'))
